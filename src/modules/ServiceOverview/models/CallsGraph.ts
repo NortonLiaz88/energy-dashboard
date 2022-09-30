5f74439b36f78b2ae9ad2cbd297ed4a145007791
Vitor_Liaz88 <vnla.lic18@uea.edu.br>
2022-09-30 15:11:37 -0400

    remove files

--- src/modules/ServiceOverview/models/CallsGraph.ts
+++
@@ -1,8 +0,0 @@
-export type CallsGraph = {
-  totalResolved: number;
-  totalUnresolved: number;
-  year: number;
-  month: number;
-  day: number;
-  date?: Date;
-};
--- src/modules/ServiceOverview/utils/ChartStyles.ts
+++
@@ -1,6 +0,0 @@
-export const ChartStyles = {
-  chart: {
-    width: '100%',
-    height: '600',
-  },
-};
